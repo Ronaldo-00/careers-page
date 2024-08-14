0cd39aa708e406d8f65f05361711150663dae60c
Ronaldo-00 <amuthuraman98@gmail.com>
2024-08-14 13:11:11 +0530

    Footer component uploaded

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Footer.css'; // footer css file
+const Footer = () => {
+  return (
+    <div>
+        <div className="orange-container">
+            <h3>Let's work together</h3>
+            <p>Whether you are a fresher or an experienced professional,
+                explore Pinnacle Infotech careers for opportunities to grow, learn, and lead.
+            </p>
+            <a href='#' className='btn-white'>Ask us any questions</a>
+        </div>
+        <div className="footer">
+            <div className="copyrights">
+                <span>© 2024 Developed by Muthu Raman.</span>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
