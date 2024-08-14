f3cd4a5ff8370832548032b5a78659a8ff125e21
Ronaldo-00 <amuthuraman98@gmail.com>
2024-08-14 12:56:06 +0530

    App.jsx file is updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import "./App.css"
 import Main from './components/Main/Main'
 import Container from './components/Container/Container'
+import Footer from './components/Footer/Footer'
 const App = () => {
   return (
     <div>
       <Main></Main>
       <Container></Container>
+      <Footer></Footer>
     </div>
   )
 }
